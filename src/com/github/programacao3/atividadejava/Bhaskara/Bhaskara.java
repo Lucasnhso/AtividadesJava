516baed9000fca1e252017d8e2d2f861a8fc7406
Lucasnhso <lucashenrique.d.s.o@gmail.com>
2020-02-27 12:57:35 -0300

    classificação pronta

--- src/com/github/programacao3/atividadejava/Bhaskara/Bhaskara.java
+++ src/com/github/programacao3/atividadejava/Bhaskara/Bhaskara.java
@@ -4,12 +4,13 @@ public class Bhaskara {
     float valorA, valorB, valorC;
 
     public Bhaskara(float num1, float num2, float num3){
-
+        calcX();
     }
 
     public float calcDelta(float a, float b, float c){
         return((b*b)*(-4*a*c));
     }
+
     public String calcX(float a, float b, float c){
         float x1, x2;
         x1 = (-b + calcDelta(a,b,c))/2;
@@ -17,7 +18,13 @@ public class Bhaskara {
         return("X linha =" + x1 + "\nX duas linhas = " + x2);
     }
 
-    public String classificacao(){
-
+    public String classificacao(float a, float b, float c){
+        if (this.calcDelta(a,b,c) < 0){
+            return ("A equação do segundo grau não possui raízes reais");
+        }
+        else if (this.calcDelta(a,b,c) == 0){
+            return("A equação do segundo grau possui uma raiz real");
+        }
+        return("A equação do segundo grau possui duas raízes reais");
     }
 }
